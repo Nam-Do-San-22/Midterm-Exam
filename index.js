ce24324cbc0c8b316745523e2b1ca2bf4c578159
odemaster <60938683+odemaster@users.noreply.github.com>
2020-11-18 19:25:10 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function getInputValue(){
            
-    var inputVal1 = document.getElementById("myInput1").value;
-    var inputVal2 = document.getElementById("myInput2").value;
-    var inputVal3 = document.getElementById("myInput3").value;
+    var inputVal1 =+document.getElementById("myInput1").value;
+    var inputVal2 =+document.getElementById("myInput2").value;
+    var inputVal3 =+document.getElementById("myInput3").value;
    
    
-    for ( inputVal1; inputVal1 <=inputVal2; inputVal1++) {
+    for (var i = inputVal1; i <= inputVal2; i++) {
 
-    var result = inputVal1*inputVal3;
-    document.write(inputVal1+ ' x ' + inputVal3 +" = "+ result +"<br>" )
+    var result = i * inputVal3;
+    document.write(i + ' x ' + inputVal3 +" = "+ result +"<br>" )
 
 
     }
