7952cf7ae985f864f6d12d831b9adb8070e52c23
odemaster <60938683+odemaster@users.noreply.github.com>
2020-11-19 10:24:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function getInputValue(){
            
-    var inputVal1 =document.getElementById("myInput1").value;
-    var inputVal2 =document.getElementById("myInput2").value;
-    var inputVal3 =document.getElementById("myInput3").value;
+    var inputVal1 =+document.getElementById("myInput1").value;
+    var inputVal2 =+document.getElementById("myInput2").value;
+    var inputVal3 =+document.getElementById("myInput3").value;
    
    
     for (var i = inputVal1; i <= inputVal2; i++) {
